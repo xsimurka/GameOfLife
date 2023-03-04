db17f6e4875374c955b5ed8afdc1e71442bc1b86
Oto Stanko <493068@fi.muni.cz>
2023-03-04 12:10:08 +0100

    Runnable file with basic print of the read file

--- gameoflife.hs
+++ gameoflife.hs
@@ -1,7 +1,7 @@
-{-# LANGUAGE NamedFieldPuns #-}
-
+--{-# LANGUAGE NamedFieldPuns #-}
+module Main where
 import Data.Set (empty, union, fromList, Set)
-import System.Console.ANSI
+import System.Console.ANSI ()
 import System.IO
 import Text.Read (readMaybe)
 import Control.Monad (guard)
@@ -21,7 +21,7 @@ parsePlanSize lines = do
 
 parseInitialState :: [String] -> Int -> Set (Int, Int)
 parseInitialState [] _= empty
-parseInitialState (h:t) r = union (parseInitialState' h r) (parseInitialState t (r + 1))
+parseInitialState (h:t) r = parseInitialState' h r `union` parseInitialState t (r + 1)
     
 parseInitialState' :: String -> Int -> Set (Int, Int)
 parseInitialState' row index = fromList [(index, col) | col <- [0.. length row - 1], (!!) row col == '#']
@@ -30,9 +30,16 @@ parseInputFile :: FilePath -> IO (Maybe GamePlan)
 parseInputFile filepath = do
     handle <- openFile filepath ReadMode
     contents <- hGetContents handle
+    print contents
     hClose handle
     let plan = lines contents
-    let (width, height) = parsePlanSize $ take 2 plan
+    let Just (width, height) = parsePlanSize $ take 2 plan
     
     let initState = parseInitialState (drop 2 plan) 0
     return $ Just GamePlan { width = width, height = height, field = initState }
+
+
+main = do
+    x <- getLine
+    gamePlan <- parseInputFile x
+    print gamePlan
