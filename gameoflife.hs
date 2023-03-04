d2a31fef7c2ba64de16b0699a28ed7600160cffd
Oto Stanko <493068@fi.muni.cz>
2023-03-04 20:20:18 +0100

    New state from the old state + terminal transition

--- gameoflife.hs
+++ gameoflife.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE NamedFieldPuns #-}
 module Main where
-import Data.Set (empty, union, fromList, Set, elemAt, deleteAt)
-import System.Console.ANSI (clearScreen, setCursorPosition)
+import Data.Set (empty, union, fromList, Set, elemAt, deleteAt, member, singleton )
+import System.Console.ANSI (clearScreen, setCursorPosition, hideCursor)
 import System.IO
+import Data.Maybe
 import Text.Read (readMaybe)
 import Control.Monad (guard)
+import Control.Concurrent (threadDelay)
 
 data GamePlan = GamePlan { width :: Int
                          , height :: Int
@@ -34,27 +36,84 @@ parseInputFile filepath = do
     hClose handle
     let plan = lines contents
     let Just (width, height) = parsePlanSize $ take 2 plan
-    
     let initState = parseInitialState (drop 2 plan) 0
     return $ Just GamePlan { width = width, height = height, field = initState }
 
 
-printToTerminal:: Maybe GamePlan -> IO ()
-printToTerminal (Just (GamePlan {width, height, field})) = do
+printToTerminal:: GamePlan -> IO ()
+printToTerminal (GamePlan {width, height, field}) = do
     if not (null field) then do
         let (x, y) = elemAt 0 field
         setCursorPosition x y
         putStrLn "#"
         let newField = deleteAt 0 field
-        printToTerminal (Just (GamePlan {width = width, height = height, field = newField}))
+        printToTerminal (GamePlan {width = width, height = height, field = newField})
                       else do return ()
-printToTerminal Nothing = do
-    putStrLn "Wrong input"
+
+isAlive:: Set (Int, Int) -> (Int, Int) -> (Int, Int) -> Bool
+isAlive inputSet (w, h) (x, y) =
+    if x > w then False else
+    if x < 0 then False else
+    if y < 0 then False else
+    if y > h then False else
+    member (x, y) inputSet
+
+numTimesFound :: Ord a => a -> [a] -> Int
+numTimesFound _ [] = 0
+numTimesFound x xs = (length . filter (== x)) xs
+
+countAdjascentAlive:: Set (Int, Int) -> (Int, Int) -> (Int, Int) -> Int
+countAdjascentAlive inputSet (w, h) (x, y) = do
+    let positions = [(x-1, y-1), (x, y-1), (x+1, y-1), (x-1, y), (x+1, y), (x-1,y+1), (x,y+1), (x+1,y+1)]
+    numTimesFound True ( map (isAlive inputSet (w, h)) positions)
+
+nextPosition:: (Int, Int) -> (Int, Int) -> Maybe (Int, Int)
+nextPosition (w, h) (x, y) =
+    if x+1 < w 
+        then Just (x+1, y)
+        else if y+1 < h
+            then Just (0, y+1)
+            else Nothing
+
+nextGamePlan:: GamePlan -> GamePlan
+nextGamePlan (GamePlan {width, height, field}) = do
+    let newField = nextState field (width, height) (0, 0) empty
+    GamePlan {width = width, height = height, field = newField}
+
+
+nextState:: Set (Int, Int) -> (Int, Int) -> (Int, Int) -> Set (Int, Int) -> Set (Int, Int)
+nextState inputSet (w, h) (x, y) buildingSet = do
+    let adjAlive = countAdjascentAlive inputSet (w, h) (x, y)
+    let nextPos = nextPosition (w, h) (x, y)
+    if member (x, y) inputSet 
+        then do
+            let nextSet = if adjAlive == 2 || adjAlive == 3
+                then buildingSet `union` (singleton (x, y))
+                else buildingSet
+            if nextPos == Nothing
+                then nextSet
+                else nextState inputSet (w, h) (fromJust nextPos) nextSet
+        else do
+            let nextSet = if adjAlive == 3
+                then buildingSet `union` (singleton (x, y))
+                else buildingSet
+            if nextPos == Nothing
+                then nextSet
+                else nextState inputSet (w, h) (fromJust nextPos) nextSet
+
+loop:: GamePlan -> IO ()
+loop gamePlan = do
+    clearScreen
+    printToTerminal gamePlan
+    threadDelay 200000
+    loop (nextGamePlan gamePlan)
 
 
 main = do
     x <- getLine
     gamePlan <- parseInputFile x
-    clearScreen
-    printToTerminal gamePlan
-    --print gamePlan
+    hideCursor
+    loop (fromJust gamePlan)
+    --if gamePlan == Nothing
+    --    then return ()
+    --    else loop (fromJust gamePlan)
